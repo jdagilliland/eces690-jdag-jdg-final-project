346632a75d291c2f5ebbcf6bc0e455be75f39e0a
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-05-28 20:19:32 -0400

    Add a script to run the oases pipeline
    It is not yet fully functional, but rather the call to oases_pipeline.py is
    somehow wrong.

---
+++ runoases.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -M jdg77@drexel.edu
+#$ -P nsftuesPrj
+#$ -pe openmpi_ib 16
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=16G
+##$ -pe shm 32-64 #for parallel processing
+#$ -l mem_free=8G
+# select the queue all.q, using hostgroup @intelhosts
+#$ -q all.q@@amdhosts
+
+PATH=/mnt/HA/groups/nsftuesGrp/.local/bin:$PATH
+PATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest/bin:$PATH
+PROJPATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest
+OASESPATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest/oases/scripts
+
+
+# ---- Keep the following
+. /etc/profile.d/modules.sh
+module load shared
+module load proteus
+module load sge/univa
+module load gcc/4.8.1
+
+# Load velvet module.
+module load velvet/1.2.10
+# Load oases?
+# Load Python
+module load python/2.7-current
+# ---- Keep the foregoing
+
+# Set up variables to keep organized.
+RAWPATH=$PROJPATH/data/rawread
+INDATADIR="${TMP}/rawread"
+mkdir -p "$INDATADIR"
+OUTDATADIR="${TMP}/oases-shortkout"
+mkdir -p "$OUTDATADIR"
+COLLECTDATADIR="$PROJPATH/oases_data/oases-shortkout"
+mkdir -p "$COLLECTDATADIR"
+# KMER=15
+
+# Download the requisite data.
+ls $RAWPATH/*.fastq
+# cp $RAWPATH/*.fastq $INDATADIR
+# ls $INDATADIR/*.fastq
+# velveth $OUTDATADIR $KMER -fastq.gz -longPaired $INDATADIR/*.fastq.gz
+python $OASESPATH/oases_pipeline.py -m 21 -M 25 -o $OUTDATADIR/ \
+	-d  -longPaired $RAWPATH/*.fastq  \
+	-p  -fastq
+
+# Clean up, clean up, everybody clean up.
+ls -l $OUTDATADIR
+mv $OUTDATADIR/* $COLLECTDATADIR/
+rmdir $OUTDATADIR
+
