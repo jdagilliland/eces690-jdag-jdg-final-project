7c2906c224e06681a50f44e11a7347b30e398ab8
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-06-01 14:54:34 -0400

    Working script to run DGE analysis

--- deg-analysis.r
+++ deg-analysis.r
@@ -1,24 +1,18 @@
-group <- factor(c('H', 'P', 'U', 'C','H', 'P', 'U', 'C','H', 'P', 'U',
+library('edgeR')
+groups <- factor(c('H', 'P', 'U', 'C','H', 'P', 'U', 'C','H', 'P', 'U',
     'C','H', 'P', 'U', 'C', 'F', 'F', 'F'))
 id <- c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
     'O', 'P', 'Q', 'R', 'S')
 rootpath <- paste0('/mnt/HA/groups/nsftuesGrp/data/',
     'gilliland-guest/express-quant/JRYH001')
-expdata <- data.frame(row.names=id)
-fname <- file.path(paste0(rootpath, id[1]), 'results.xprs')
-x <- read.delim(fname, row.names="target_id")
-# order(x)
-genenames <- row.names(x)
-# genenames <- genenames[order(genenames)]
+fnames <- c()
 for (i in 1:length(id)) {
     fname <- file.path(paste0(rootpath, id[i]), 'results.xprs')
-    i
-    sampleexp <- read.delim(fname, row.names="target_id")
-    explevel <- sampleexp['eff_counts']
-    # sampleexp['eff_counts']
-    expdata[i, 1] <- id[i]
-    expdata[i, 2] <- group[i]
-    # expdata[i, seq(3, 2+length(genenames))] <- aperm(sampleexp['eff_counts'],
-    #     c(2, 1))
+    fnames[i] <- fname
 }
-names(expdata) <- c('ID', 'treatment', genenames)
+expdata <- readDGE(fnames, columns=c(1, 8), group=groups, labels=id)
+expdata <- calcNormFactors(expdata)
+expdata <- estimateCommonDisp(expdata)
+expdata <- estimateTagwiseDisp(expdata)
+et <- exactTest(expdata)
+topTags(et)
---
+++ run-dge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -M jdg323@drexel.edu
+#$ -P nsftuesPrj
+#$ -pe openmpi_ib 1
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=16G
+##$ -pe shm 32-64 #for parallel processing
+#$ -l mem_free=8G
+# select the queue all.q, using hostgroup @intelhosts
+#$ -q all.q@@amdhosts
+
+PATH=/mnt/HA/groups/nsftuesGrp/.local/bin:$PATH
+PATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest/bin:$PATH
+PROJPATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest
+
+# ---- Keep the following
+. /etc/profile.d/modules.sh
+module load shared
+module load proteus
+module load sge/univa
+module load gcc/4.8.1
+
+# ---- Keep the foregoing
+
+Rscript deg-analysis.r
