70f12aae844aefd8b449e0b886ab594fe0a9ed79
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-06-08 16:50:23 -0400

    Improve analysis of differential gene expression

--- deg-analysis.r
+++ deg-analysis.r
@@ -10,12 +10,26 @@ for (i in 1:length(id)) {
     fname <- file.path(paste0(rootpath, id[i]), 'results.xprs')
     fnames[i] <- fname
 }
-expdata <- readDGE(fnames, columns=c(1, 8), group=groups, labels=id)
+print('Loading expression data...')
+expdata <- readDGE(fnames, columns=c(2, 8), group=groups, labels=id)
+print('Done!')
 expdata <- calcNormFactors(expdata)
 expdata <- estimateCommonDisp(expdata)
 expdata <- estimateTagwiseDisp(expdata)
+
+# Try exact tests between pairs
+print('Performing exact test')
+et <- exactTest(expdata, pair=c('H', 'P'))
+print('Top tags for exact test between control diet and pollen')
+topEt <- topTags(et)
+topEt
+
+# Try a GLM approach
+print('Fitting a GLM')
 design <- model.matrix(~groups)
 fit <- glmFit(expdata, design)
 colnames(fit)
 lrt.all <- glmLRT(fit, coef=2:5)
-topTags(lrt.all)
+print('Top tags for GLM between all groups')
+topGlm <- topTags(lrt.all)
+topGlm
