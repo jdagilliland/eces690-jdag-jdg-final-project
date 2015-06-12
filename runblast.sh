d8c48849d9d867294d5271ed7822642478f2a380
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-06-12 02:37:13 -0400

    Add BLAST script that is not quite functioning

---
+++ runblast.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -M jdg323@drexel.edu
+#$ -P nsftuesPrj
+#$ -pe openmpi_ib 64
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=16G
+##$ -pe shm 32-64 #for parallel processing
+#$ -l mem_free=8G
+# select the queue all.q, using hostgroup @intelhosts
+#$ -q all.q@@amdhosts
+
+PATH=/mnt/HA/groups/nsftuesGrp/.local/bin:$PATH
+PATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest/bin:$PATH
+PROJPATH=/mnt/HA/groups/nsftuesGrp/data/gilliland-guest
+
+# ---- Keep the following
+. /etc/profile.d/modules.sh
+module load shared
+module load proteus
+module load sge/univa
+module load gcc/4.8.1
+
+module add ncbi-blast/gcc/64/2.2.30
+# ---- Keep the foregoing
+
+# Set up variables to keep organized.
+DATAPATH=$PROJPATH/data
+
+# Perform blast on differentially expressed genes.
+blastn -query $DATAPATH/degenes.fa -out $DATAPATH/degenes-id.txt
